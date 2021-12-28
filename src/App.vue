bdb774a0f4842c9ab6ae69d9ffcbd70c40db2c52
rusahbhd97 <rushabhd97@gmail.com>
2021-12-28 19:00:28 +0530

    First Page

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,48 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Stocks
+    v-if="mainData.length !== 0" 
+    v-bind:mainData="mainData"
+  />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Stocks from './components/Stocks.vue';
+import axios from 'axios';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    Stocks
+  },
+  data() {
+    return {
+      mainData: [],
+    }
+  },
+  created() {
+    this.getData();
+  },
+  methods: {
+    async getData() {
+      axios.get('https://mobile-app-challenge.herokuapp.com/data')
+      .then(({ data }) => {
+        this.setData(data);
+      });
+    },
+    setData(data) {
+      this.mainData = data;
+    }
+  },
 }
 </script>
 
-<style>
+<style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  /* margin-top: 60px; */
 }
 </style>
---
+++ src/components/Stocks.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="body">
+    <div class="phone-section">
+      <ul class="text-left mb-4 headers">
+        <li v-for="(d,i) in stocks" :key="i">
+          <a>
+            <div>{{d.name}}</div>
+            <div :class="'subtext '+d.color">{{d.tag}}</div>
+          </a>
+        </li>
+      </ul>	
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: ['mainData'],
+  data() {
+    return {
+      stocks: this.mainData,
+    }
+  },
+}
+</script>
+<style lang="scss">
+.body {
+  display: -ms-flexbox;
+  display: flex;
+  -ms-flex-align: center;
+  align-items: center;
+  padding-top: 40px;
+  padding-bottom: 40px;
+}
+.phone-section {
+  width: 100%;
+  max-width: 400px;
+  padding: 15px;
+  margin: auto;
+  list-style: none;
+  background-color: #01131B;
+}
+ul {
+  text-align: left;
+  padding-left: 5px;
+  margin-top: 0;
+  margin-bottom: 10px;
+}
+li {
+  padding: 10px;
+  border-bottom: 1px dotted #ececec;
+  color: #fff;
+  font-size: 14px;
+  .subtext {
+    font-size: 10px;
+    &.green {
+      color: green;
+    }
+    &.red {
+      color: red;
+    }
+  }
+}
+</style>
